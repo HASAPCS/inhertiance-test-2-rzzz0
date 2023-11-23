f7851669a38dad4c81f8965c0eeeca3865d96aad
rzzz0 <rudyhsurh@gmail.com>
2023-11-23 10:16:11 +0800

    appliance

--- src/main/java/com/example/Appliance.java
+++ src/main/java/com/example/Appliance.java
@@ -1,33 +1,46 @@
-package com.example;
-
-public class Appliance {
-    
-
-    // Constructor to initialize an ElectronicDevice object
-    public Appliance(String brand, String model) {
-        // TODO: Initialize the common attributes (brand, model) with the provided parameters
-    }
-
-    // TODO: Implement getters (and setters if necessary) for the brand and model.
-
-    // OPTIONALLY implement the required methods to pass the BonusTest.
-
-}
 
 package com.example;
 
 public class Appliance {
+    private String brand;
+    private String model;
+    private int powerUsage;
+
     // Attributes
     // TODO: Declare common attributes for electronic devices, such as brand and model and powerUsage.
 
     // Constructor
     public Appliance(String brand, String model, int powerUsage) {
         // TODO: Initialize all attributes
+        this.brand = brand;
+        this.model = model;
+        this.powerUsage = powerUsage;
     }
 
     // Getters
     // TODO: Implement getters for brand, model, and powerUsage
+    public String getBrand(){
+        return this.brand;
+    }
+    public String getModel(){
+        return this.model;
+    }
+    public int powerUsage(){
+        return this.powerUsage;
+    }
+    public void setBrand(String newBrand){
+        this.brand = newBrand;
+    }
+    public void setModel(String newModel){
+        this.model = newModel;
 
+    }
+    public void setPowerUsage(int newPowerUsage){
+        this.powerUsage = newPowerUsage;
+    }
+    public String displayInfo(){
+        return "Brand: " + this.brand + ", " + "Model: " + this.model + ", " + "Power Usage: " + this.powerUsage + " Watts";
+    }
     // displayInfo Method
     // TODO: Implement a method that returns information about the appliance, including brand, model, and power usage.
     // Check the tests for more details
